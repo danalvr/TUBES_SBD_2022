8cdb564ee0022b18c833d8f674b0ac319153845c
Daniel Alvaro <danielkisaran@gmail.com>
2022-12-03 18:39:40 +0700

    update databaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Level;
+use App\Models\Book;
+use App\Models\Customer;
+use App\Models\Transaction;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +18,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Level::create([
+            'level_name' => 'central admin',
+            'description' => 'Have a full access on dashboard',
+        ]);
+        Level::create([
+            'level_name' => 'Supplier Admin',
+            'description' => 'Have access on suplier dashboard',
+        ]);
+        Level::create([
+            'level_name' => 'Cashier Admin',
+            'description' => 'Have access on cashier dashboard',
+        ]);
+        
+        User::create([
+            'name' => 'Daniel Alvaro',
+            'username' => 'danalvr_',
+            'email' => 'danielalvaro@students.undip.ac.id',
+            'password' => '$2a$12$IW.4ncypZhcVnAuf0S5PyuPpyLxBF9Q2yHnpTeeIEE8RLkh.CEMN6',
+            'level_id' => 1
+        ]);
+        User::create([
+            'name' => 'Budiman Santoso',
+            'username' => 'budimans6',
+            'email' => 'santosobudiman@students.undip.ac.id',
+            'password' => '$2a$12$IW.4ncypZhcVnAuf0S5PyuPpyLxBF9Q2yHnpTeeIEE8RLkh.CEMN6',
+            'level_id' => 2
+        ]);
+        User::create([
+            'name' => 'Pipit Nurhayati',
+            'username' => 'pipitnur08',
+            'email' => 'pipitnurhayati@students.undip.ac.id',
+            'password' => '$2a$12$IW.4ncypZhcVnAuf0S5PyuPpyLxBF9Q2yHnpTeeIEE8RLkh.CEMN6',
+            'level_id' => 3
+        ]);
     }
 }
