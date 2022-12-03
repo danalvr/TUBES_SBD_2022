5dc677175a948febc7ac198a97d3fe851d4e7e6a
Daniel Alvaro <danielkisaran@gmail.com>
2022-12-03 17:50:32 +0700

    make a model, migration, factory for book

---
+++ database/migrations/2022_12_03_104302_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->bigInteger('price');
+            $table->string('publisher');
+            $table->string('author');
+            $table->integer('stock_amount');
+            $table->foreignId('id_supplier');
+            $table->date('publication_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
