060454bd7a2ca65a5a47aa4bb294a1824f2c9c43
Daniel Alvaro <danielkisaran@gmail.com>
2022-12-04 17:42:51 +0700

    create LoginController and index.blade.php(Login)

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login.index', [
+            'title' => 'Login',
+            'active' => 'login'
+        ]);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email:dns',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            if (auth()->user()->level_id == 1) {
+                return redirect()->intended('/');
+            } elseif (auth()->user()->level_id == 2) {
+                return redirect()->intended('/book-stock');
+            } else {
+                return redirect()->intended('/transaction');
+            }
+        }
+
+        return back()->with('loginError', 'Login failed!');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
